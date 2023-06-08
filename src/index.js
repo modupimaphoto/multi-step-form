bd6f471c4f11c0dbe3f7d420506d89fe63456dee
modupi <modupimaphoto@gmail.com>
2023-06-08 13:32:03 +0200

    importing jquery popper.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
+import $ from 'jquery';
+import Popper from 'popper.js';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
